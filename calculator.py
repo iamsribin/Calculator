cd82840e38f4a35a8d9c042360379289ec973d6a
Sribin <sribin85@gmail.com>
2022-12-02 02:46:31 -0600

    frame added

--- calculator.py
+++ calculator.py
@@ -1,20 +1,33 @@
 import tkinter as tk
 
-class calculator:
-
-      def __init__(self):
-          self.wintdow = tk.Tk()
-          self.wintdow.title("Calculetor")
-          self.wintdow.iconbitmap("./calculator photo.ico")
-          self.wintdow.geometry("355x555")
-          self.wintdow.resizable(0,0)
+LIGHT_GRAY = "#F5F5F5"
 
 
+class Calculator:
 
-      def run(self):
-          self.wintdow.mainloop()
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Calculetor")
+        self.window.iconbitmap("./calculator photo.ico")
+        self.window.geometry("355x555")
+        self.window.resizable(0, 0)
 
+        self.display_frame = self.create_display_frame()
+        self.button_frame = self.create_button_frame()
+
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=226, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_button_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def run(self):
+        self.window.mainloop()
 
 if __name__ == "__main__":
-    calc = calculator()
+    calc = Calculator()
     calc.run()

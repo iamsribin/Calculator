f19a3d4c95aa7e43b2a4337da42e800388f8a31d
Sribin <sribin85@gmail.com>
2022-12-04 00:23:01 -0600

    changed the color

--- calculator.py
+++ calculator.py
@@ -7,11 +7,10 @@ DIGIT_FOND_STYLE = ("Arial", 24, "bold")
 DELETE_FOND_STYLE = ("Arial", 15, "bold")
 
 # COLOR
-WHITE = "#FFFFFA"
-LABEL_COLOR = "#000000"
-LIGHT_GRAY = "#F5F5F5"
+BLACK = "#161621"
+WHITE = "#FFFFFF"
 RED = "#F94040"
-
+BLUE = "#20459B"
 
 class Calculator:
     def __init__(self):
@@ -20,6 +19,8 @@ class Calculator:
         self.window.iconbitmap("./calculator photo.ico")
         self.window.geometry("375x587")
         self.window.resizable(0, 0)
+        self.window.attributes("-alpha", 0.8)
+        self.window.configure(bg=BLUE)
 
         self.total_expression = "0"
         self.current_expression = "0"
@@ -39,9 +40,9 @@ class Calculator:
 
         self.operations = {"*": "\u00d7", "-": "-", "+": "+"}
         self.create_operator_button()
+        
         self.create_clear_button()
         self.create_delete_button()
-
         self.create_equal_button()
 
         self.button_frame.rowconfigure(0, weight=1)
@@ -50,23 +51,23 @@ class Calculator:
             self.button_frame.rowconfigure(x, weight=1)
 
     def create_display_frame(self):
-        frame = tk.Frame(self.window, height=316)
+        frame = tk.Frame(self.window, height=316, bg=BLUE)
         frame.pack(expand=True, fill="both")
         return frame
 
     def create_button_frame(self):
-        frame1 = tk.Frame(self.window, cursor="hand2")
+        frame1 = tk.Frame(self.window, cursor="hand2", bg=BLUE)
         frame1.pack(expand=True, fill="both")
         return frame1
 
     # ----------------------------------------------------------------------------------------
 
     def create_display_label(self):
-        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY,
-                               fg=LABEL_COLOR, padx=24, font=SMALL_FOND_STYLE)
+        total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=BLUE,
+                               fg=WHITE, padx=24, font=SMALL_FOND_STYLE)
         total_label.pack(expand=True, fill="both")
         label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E,
-                         bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FOND_STYLE)
+                          bg=BLUE, fg=WHITE, padx=24, font=LARGE_FOND_STYLE)
         label.pack(expand=True, fill="both")
         return total_label, label
 
@@ -74,40 +75,42 @@ class Calculator:
 
     def create_digit_button(self):
         for digit, grid_value in self.digits.items():
-            button = tk.Button(self.button_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGIT_FOND_STYLE)
-            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW, padx=4, pady=4)
+            button = tk.Button(self.button_frame, text=str(digit), bg=BLACK, fg=WHITE, font=DIGIT_FOND_STYLE,
+                               activebackground=BLACK,)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW, padx=2, pady=2)
 
-        button = tk.Button(self.button_frame, text="0", bg=WHITE, fg=LABEL_COLOR, font=DIGIT_FOND_STYLE)
-        button.grid(row=4, column=1, columnspan=2, sticky=tk.NSEW, padx=4, pady=4)
+        button = tk.Button(self.button_frame, text="0", bg=BLACK, fg=WHITE, font=DIGIT_FOND_STYLE,
+                           activebackground=BLACK)
+        button.grid(row=4, column=1, columnspan=2, sticky=tk.NSEW, padx=2, pady=2)
 
     # ----------------------------------------------------------------------------------------------
 
     def create_operator_button(self):
         i = 0
         for operator, symbol in self.operations.items():
-            button = tk.Button(self.button_frame, text=str(symbol), bg=WHITE, fg=LABEL_COLOR, font=DIGIT_FOND_STYLE,
-                               activeforeground=RED)
-            button.grid(row=i, column=4, sticky=tk.NSEW, padx=4, pady=4)
+            button = tk.Button(self.button_frame, text=str(symbol), bg=BLACK, fg=WHITE, font=DIGIT_FOND_STYLE,
+                               activeforeground=RED, activebackground=BLACK)
+            button.grid(row=i, column=4, sticky=tk.NSEW, padx=2, pady=2)
             i += 1
 
-        button = tk.Button(self.button_frame, text="\u00f7", bg=WHITE, fg=LABEL_COLOR, font=DIGIT_FOND_STYLE,
-                           activeforeground=RED)
-        button.grid(row=0, column=3, sticky=tk.NSEW, padx=4, pady=4)
+        button = tk.Button(self.button_frame, text="\u00f7", bg=BLACK, fg=WHITE, font=DIGIT_FOND_STYLE,
+                           activeforeground=RED, activebackground=BLACK)
+        button.grid(row=0, column=3, sticky=tk.NSEW, padx=2, pady=2)
 
     def create_clear_button(self):
-        button = tk.Button(self.button_frame, text="C", bg=RED, fg=WHITE, font=DIGIT_FOND_STYLE,
+        button = tk.Button(self.button_frame, text="C", bg=RED, fg=BLACK, font=DIGIT_FOND_STYLE,
                            activebackground=RED, activeforeground=WHITE)
-        button.grid(row=0, column=2, sticky=tk.NSEW, padx=4, pady=4)
+        button.grid(row=0, column=2, sticky=tk.NSEW, padx=2, pady=2)
 
     def create_delete_button(self):
-        button = tk.Button(self.button_frame, text="\u232b", bg=RED, fg=WHITE, font=DELETE_FOND_STYLE,
+        button = tk.Button(self.button_frame, text="\u232b", bg=RED, fg=BLACK, font=DELETE_FOND_STYLE,
                            activebackground=RED, activeforeground=WHITE)
-        button.grid(row=0, column=1, sticky=tk.NSEW, padx=4, pady=4)
+        button.grid(row=0, column=1, sticky=tk.NSEW, padx=2, pady=2)
 
     def create_equal_button(self):
-        button = tk.Button(self.button_frame, text="=", bg=WHITE, fg=LABEL_COLOR, font=DELETE_FOND_STYLE,
-                           activebackground=RED, activeforeground=WHITE)
-        button.grid(row=3, rowspan=4, column=4, sticky=tk.NSEW, padx=4, pady=4)
+        button = tk.Button(self.button_frame, text="=", bg=BLACK, fg=WHITE, font=DELETE_FOND_STYLE,
+                           activebackground=RED, activeforeground=BLACK)
+        button.grid(row=3, rowspan=4, column=4, sticky=tk.NSEW, padx=2, pady=2)
 
         # ----------------------------------------------------------------------------------------
 
